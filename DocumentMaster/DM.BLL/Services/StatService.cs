90f2fa1e5fdb5f9e4fa56a8e55a87c2418ca502e
NewWork <evg.a.smirnov@gmail.com>
2022-10-12 17:16:02 +0300

    1'

---
+++ DocumentMaster/DM.BLL/Services/StatService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DM.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.BLL.Services
+{
+    public class StatService
+    {
+        private readonly IMapper? _mapper;
+        private readonly IDbContextFactory<DMContext>? _contextFactory;
+        public StatService(IMapper? mapper, IDbContextFactory<DMContext>? contextFactory)
+        {
+            _mapper = mapper;
+            _contextFactory = contextFactory;
+        }
+
+    }
+}
---
+++ DocumentMaster/Models/StatUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StatUserModel
+    {
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public PersonDTO? Person { get; set; }
+        public ProjectDTO? Project { get; set; }
+        public List<FileDTO> DevelopedFiles { get; set; } = new List<FileDTO>();
+        public List<ControlDTO> Controls { get; set; } = new List<ControlDTO>();
+
+    }
+}
