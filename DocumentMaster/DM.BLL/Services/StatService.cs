47f6f86820c9fd28171255772e2783e1e8c30973
NewWork <evg.a.smirnov@gmail.com>
2022-10-14 17:07:52 +0300

    1

--- DocumentMaster/DM.BLL/Services/StatService.cs
+++ DocumentMaster/DM.BLL/Services/StatService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DM.DAL.Models;
 using Microsoft.EntityFrameworkCore;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,63 @@ namespace DM.BLL.Services
     {
         private readonly IMapper? _mapper;
         private readonly IDbContextFactory<DMContext>? _contextFactory;
+
         public StatService(IMapper? mapper, IDbContextFactory<DMContext>? contextFactory)
         {
             _mapper = mapper;
             _contextFactory = contextFactory;
+        }
+
+        public async Task<StatUserModel> GetStatForPerson(int personId, DateTime dateFrom, DateTime dateTo)
+        {
+            StatUserModel model = new();
+
+            model.DateFrom = dateFrom;
+            model.DateTo= dateTo;
+
+            using var context = _contextFactory.CreateDbContext();
+            var person = context.Persons.Find(personId);
+            model.Person = _mapper.Map<PersonDTO>(person);
+
+            var files = await context.FileUnits.Where(f => f.PersonId == personId && f.Status == StatusFile.Archive)
+                .Include(c => c.Controls.Where(ch => ch.IsConfirmed))
+                .Where(f => dateFrom < f.Controls.First().DateTime && dateTo > f.Controls.First().DateTime).ToListAsync();
+
+            model.DevelopedFiles = _mapper.Map<List<FileDTO>>(files);
+
+            var controls = await context.Controls
+                .Where(c => c.IsConfirmed && c.PersonId == personId && c.DateTime > dateFrom && c.DateTime < dateTo).ToListAsync();
+            model.Controls = _mapper.Map<List<ControlDTO>>(controls);
+
+            return model;
+        }
+
+        public async Task<StatUserModel> GetStatForPersonInProject(int personId, int projectId, DateTime dateFrom, DateTime dateTo)
+        {
+            StatUserModel model = new();
+
+            model.DateFrom = dateFrom;
+            model.DateTo = dateTo;
+
+            using var context = _contextFactory.CreateDbContext();
+
+            var person = context.Persons.Find(personId);
+            model.Person = _mapper.Map<PersonDTO>(person);
+
+            var project = context.Projects.Find(projectId);
+            model.Project = _mapper.Map<ProjectDTO>(project);
+
+            var files = await context.FileUnits.Where(f => f.ProjectId==projectId && f.PersonId == personId && f.Status == StatusFile.Archive )
+                .Include(c => c.Controls.Where(ch => ch.IsConfirmed))
+                .Where(f => dateFrom < f.Controls.First().DateTime && dateTo > f.Controls.First().DateTime).ToListAsync();
+            model.DevelopedFiles = _mapper.Map<List<FileDTO>>(files);
+
+            var controls = await context.Controls.Where(c => c.IsConfirmed && c.PersonId == personId && c.DateTime > dateFrom && c.DateTime < dateTo).Include(f=>f.FileUnit)
+                .Where(t=>t.FileUnit.ProjectId==projectId).ToListAsync();
+
+            model.Controls = _mapper.Map<List<ControlDTO>>(controls);
+
+            return model;
         }
 
     }
