24fde95016e0be605176b6330a12adb4fc80d0e6
NewWork <evg.a.smirnov@gmail.com>
2022-11-10 17:08:22 +0300

    1

---
+++ DocumentMaster/DM.DAL/Migrations/20221110115550_OldVersions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DM.DAL.Migrations
+{
+    public partial class OldVersions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOldVersion",
+                table: "FileUnits",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOldVersion",
+                table: "FileUnits");
+        }
+    }
+}
