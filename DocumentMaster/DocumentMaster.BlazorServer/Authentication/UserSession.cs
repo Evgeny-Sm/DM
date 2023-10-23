e1aa83a7a40407885233412a3da3d6afbaefad43
Evgeniy Smirnov <evg.a.smirnov@gmail.com>
2023-10-23 22:00:11 +0300

    1

--- DocumentMaster/DocumentMaster.BlazorServer/Authentication/UserSession.cs
+++ DocumentMaster/DocumentMaster.BlazorServer/Authentication/UserSession.cs
@@ -4,6 +4,8 @@
     {
         public string UserName { get; set; }
         public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int PersonId { get; set; }
     }
 }
